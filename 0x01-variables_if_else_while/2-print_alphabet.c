2ab33674d0d9b5e64a0d320322954ecd998311e9
salim Tasiu Ibrahim <tasiusalim260@gmailcom>
2022-06-11 10:39:17 -0700

    created 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabet in lowercase
+* Return: Always 0 (Success)
+*/
+iint main(void)
+{
+	char alpha
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+		piutchar('\n');
+	return (0);
+}
+
