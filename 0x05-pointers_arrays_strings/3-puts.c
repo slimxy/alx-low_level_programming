79431c32271ddff5c6ae498099709e509258e00a
salim Tasiu Ibrahim <tasiusalim260@gmailcom>
2022-06-13 20:25:07 -0700

    created 5-rev_string.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
 * _puts - prints string
 * @str: string
 * Return: void
 */
-
 void _puts(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putschar(str[i]);
+		putchar(str[i]);
 		i++;
 	}
-	_putschar('\n');
+	putchar('\n');
 }
