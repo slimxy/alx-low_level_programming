a405dcd94a610faa04cf14004bc8425e617f0152
salim Tasiu Ibrahim <tasiusalim260@gmailcom>
2022-06-17 14:01:41 -0700

    created 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
-* main - main
+* main - remove infinte loop
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
 
+	printf("Infinite loop incoming :(\n");
 	i = 0;
-
-	positive_or_negative(i);
-
+	/** while (i < 10) */
+	{
+		putchar(i);
+	}
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
-
 }
