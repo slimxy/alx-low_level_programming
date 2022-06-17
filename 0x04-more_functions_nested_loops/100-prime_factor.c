e1c3c56ae2a4aa40b0327c60909d1ec5efd8c099
salim Tasiu Ibrahim <tasiusalim260@gmailcom>
2022-06-17 13:36:58 -0700

    created 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+int main(void)
+{
+	unsigned long int a = 3, n = 612852475143;
+
+	for (; a < 12057; a += 2)
+	{
+		while (n % a == 0 && n != a)
+			n /= a;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
