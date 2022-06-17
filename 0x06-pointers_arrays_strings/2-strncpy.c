b10c2aa26649e34681b87cd4604bb94d9483d26e
salim Tasiu Ibrahim <tasiusalim260@gmailcom>
2022-06-16 20:57:21 -0700

    created 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _strcmp - compares two strings
-* @s1: first string.
-* @s2: second string.
-* Return: 0 if s1 and s2 are equals,
-* another number if not.
-*/
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
