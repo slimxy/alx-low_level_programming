8aa0ae76a4a1d4d36d90274208e51f23ffc97601
salim Tasiu Ibrahim <tasiusalim260@gmailcom>
2022-06-17 13:26:12 -0700

    created 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry piont
+* Return: Always 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 1 ; a < 100 ; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz ");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", a);
+	}
+	printf("Buzz\n");
+	return (0);
+}
